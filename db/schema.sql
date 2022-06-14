8155d4b23b5bf2ba4da4a402f1368e254784065d
alanam79 <halo2382001@gmail.com>
2022-06-14 19:50:27 -0400

    adding database calls

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS departments;
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
--- server.js
+++ server.js
@@ -15,18 +15,51 @@ app.use(express.json());
 //   });
 // });
 
+//  get all departments
+// db.query(`SELECT * FROM departments`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+// get one department
+// db.query(`SELECT * FROM departments WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// delete one department
+// db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// add a department
+const sql = `INSERT INTO departments (id, department_name)
+                VALUES (?,?)`;
+
+const params = [1, 'Executive'];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 // Default response for any other request (not found), this is a very important catchall, so must be the last route
 app.use((req, res) => {
   res.status(404).end();
 });
 
 // Start server after DB connection
-// db.connect((err) => {
-//     if (err) throw err;
-//     console.log("Database connected.❤️");
+db.connect((err) => {
+    if (err) throw err;
 
 // starts server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}🚀`);
 });
-//   });
+  });
