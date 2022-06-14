1b947d974c02970a17772ffb6287af5ce2d3a000
alanam79 <halo2382001@gmail.com>
2022-06-14 19:03:06 -0400

    added first table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (department_name)
+VALUES
+    ('Executive'),
+    ('Sales'),
+    ('Accounting'),
+    ('Marketing'),
+    ('Dev Team'),
+    ('Legal Team'),
+    ('Human Resources');
