4a3320980fa29fb9bff680c2e0295e7b11e9f978
alanam79 <halo2382001@gmail.com>
2022-06-15 22:38:47 -0400

    working on role table to add titles

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,22 @@
 INSERT INTO department (id, name)
     VALUES
     (1, 'CEO aka HBIC'),
-    (2, 'Sales Manager'),
-    (3, 'Salesperson'),
-    (4, 'Accounting Manager'),
-    (5, 'Accountant'),
-    (6, 'Marketing Manager'),
-    (7, 'Marketer'),
-    (8, 'Dev Team'),
-    (9, 'Legal Team'),
-    (10, 'Human Resources');
+    (2, 'Sales'),
+    (3, 'Accounting'),
+    (4, 'Marketing'),
+    (5, 'Dev Team'),
+    (6, 'Legal Team'),
+    (7, 'Human Resources');
+
+INSERT INTO role (id, title, salary, department_id)
+    VALUES
+    (1, 'CEO aka HBIC', 1000000, 1),
+    (2, 'Sales Manager', 190000, 2),
+    (3, 'Salesperson', 50000, 2),
+    (4, 'Accounting Manager', 85000, 3),
+    (5, 'Accountant', 65000, 3),
+    (6, 'Marketing Manager', 40000, 4),
+    (7, 'Marketer', 45000, 4),
+    (8, 'Dev Team', 70000, 5),
+    (9, 'Legal Team', 85000, 6),
+    (10, 'Human Resources', 75000, 7);
