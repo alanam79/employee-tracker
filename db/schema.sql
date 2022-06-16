99410b97c2d00415b4c0b36fe4c692a27a948f3c
alanam79 <halo2382001@gmail.com>
2022-06-15 23:26:06 -0400

    working on foreign keys for employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE department (
     id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -7,5 +11,16 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NULL
+    department_id INT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL
+    -- CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) DELETE CASCADE,
+    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -20,3 +20,17 @@ INSERT INTO role (id, title, salary, department_id)
     (8, 'Dev Team', 70000, 5),
     (9, 'Legal Team', 85000, 6),
     (10, 'Human Resources', 75000, 7);
+
+    INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+    VALUES
+    (1, 'Alana', 'McKeel', 1, NULL),
+    (2, 'Shaun', 'Greene', 2, 1),
+    (3, 'Kevin', 'Begin', 3, 2),
+    (4, 'Cabral', 'Williams', 4, 1),
+    (5, 'Brennan', 'Spicer', 5, 4),
+    (6, 'Katie', 'TA', 6, 1),
+    (7, 'Jim', 'Gryck', 7, 6),
+    (8, 'Katie', 'McKeel', 8, 1),
+    (9, 'Alissa', 'Smith', 9, 10),
+    (10, 'Don', 'Ruth', 10, 9);
+
